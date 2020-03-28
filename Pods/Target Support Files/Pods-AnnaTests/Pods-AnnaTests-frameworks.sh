acf47b2f81c7a4beaa25a6fadfcd91ce1155a0b0
Michal Mizera <1938540+Axadiw@users.noreply.github.com>
2020-03-28 13:43:55 +0100

    Włączenie frameworków w podfile (firebase nadal występuje w binarce)

--- Pods/Target Support Files/Pods-AnnaTests/Pods-AnnaTests-frameworks.sh
+++ Pods/Target Support Files/Pods-AnnaTests/Pods-AnnaTests-frameworks.sh
@@ -197,7 +197,6 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug_dev" ]]; then
-  install_framework "${PODS_ROOT}/BugfenderSDK/BugfenderSDK.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
@@ -213,7 +212,6 @@ if [[ "$CONFIGURATION" == "Debug_prod" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
 fi
 if [[ "$CONFIGURATION" == "Release_dev" ]]; then
-  install_framework "${PODS_ROOT}/BugfenderSDK/BugfenderSDK.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GoogleUtilities/GoogleUtilities.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PromisesObjC/FBLPromises.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/nanopb/nanopb.framework"
