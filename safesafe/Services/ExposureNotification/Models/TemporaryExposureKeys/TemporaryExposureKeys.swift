2421abb25036e971e121effa41ea18d2401ebbb8
Mateusz Podgórski <kontakt@mpodgorski.pl>
2020-05-20 10:19:32 +0200

    [PSAFE-721] Remove unused fields in TemporaryExposureKeys

--- safesafe/Services/ExposureNotification/Models/TemporaryExposureKeys/TemporaryExposureKeys.swift
+++ safesafe/Services/ExposureNotification/Models/TemporaryExposureKeys/TemporaryExposureKeys.swift
@@ -11,17 +11,13 @@ struct TemporaryExposureKeys: Encodable {
     enum Default {
         static let regions = ["PL"]
         static let platform = "ios"
-        static let transmissionRisk = 8 // TODO: verify value
         static let appPackageName = Bundle.main.bundleIdentifier!
-        static let verificationAuthorityName = "GIS"
     }
     
     let temporaryExposureKeys: [TemporaryExposureKey]
     let regions = Default.regions
     let platform = Default.platform
-    let transmissionRisk = Default.transmissionRisk
     let appPackageName = Default.appPackageName
-    let verificationAuthorityName = Default.verificationAuthorityName
     let verificationPayload: String
     let deviceVerificationPayload: String
     
