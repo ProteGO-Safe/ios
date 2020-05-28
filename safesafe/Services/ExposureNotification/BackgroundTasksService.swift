9d9e8e1309f4d93eccbfd6731ca7e6fcb658c69b
Rafal Malczynski <rafal.malczynski@gmail.com>
2020-05-28 17:41:52 +0200

    [] Update BG task identifier in the background

--- safesafe/Services/ExposureNotification/BackgroundTasksService.swift
+++ safesafe/Services/ExposureNotification/BackgroundTasksService.swift
@@ -18,7 +18,7 @@ final class BackgroundTasksService: BackgroundTasksServiceProtocol {
     
     // MARK: - Properties
     
-    private let backgroundTaskID = "protego.safe.backgroundTask.exposure-notification"
+    private let backgroundTaskID = "pl.gov.mc.protegosafe.backgroundAnalysis.exposure-notification"
     private let exposureService: ExposureServiceProtocol
     
     // MARK: - Life Cycle
