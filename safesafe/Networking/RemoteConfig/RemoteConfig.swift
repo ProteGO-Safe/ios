c01ed090ce712fe314f661581ffd9953af6ec2a1
lukasz szyszkowski <lukasz.szyszkowski@holdapp.pl>
2020-06-05 02:09:53 +0200

    [] Added timeout for remote configuration download
    [] Fix for detectExposures (renaming files on download)

--- safesafe/Networking/RemoteConfig/RemoteConfig.swift
+++ safesafe/Networking/RemoteConfig/RemoteConfig.swift
@@ -29,35 +29,50 @@ final class RemoteConfiguration: RemoteConfigProtocol {
             self.remoteConfig.configSettings = settings
         }
     }
-
+    
     func configuration() -> Promise<RemoteConfigurationResponse> {
-        return Promise { seal in
-            remoteConfig.fetchAndActivate { [weak self] status, error in
-                guard let self = self else {
-                    seal.reject(InternalError.deinitialized)
-                    return
-                }
-                
-                if let error = error {
-                    seal.reject(error)
+        let timeout = after(seconds: 5)
+        let config = fetchConfiguration()
+        return race(config.asVoid(), timeout.asVoid())
+            .then { _ -> Promise<RemoteConfigurationResponse> in
+                if timeout.isResolved {
+                    return .init(error: InternalError.timeout)
+                } else if let value = config.value {
+                    return .value(value)
                 } else {
-                    switch status {
-                    case .successFetchedFromRemote, .successUsingPreFetchedData:
-                        do {
-                            let diagnosis: DiagnosisKeyDownloadConfiguration = try self.decodeConfiguartion(key: .diagnosisKeyConfig)
-                            let exposure: ExposureConfiguration = try self.decodeConfiguartion(key: .exposureConfiguration)
-                            seal.fulfill(RemoteConfigurationResponse(diagnosis: diagnosis, exposure: exposure))
-                        } catch {
-                            seal.reject(error)
-                        }
-                    case .error:
-                        seal.reject(InternalError.remoteActivate)
-                    @unknown default:
-                        seal.reject(InternalError.remoteUnknownStatus)
-                    }
+                    return .init(error: InternalError.nilValue)
                 }
-            }
         }
+    }
+    
+    private func fetchConfiguration() -> Promise<RemoteConfigurationResponse> {
+        return Promise { seal in
+                   remoteConfig.fetchAndActivate { [weak self] status, error in
+                       guard let self = self else {
+                           seal.reject(InternalError.deinitialized)
+                           return
+                       }
+                       
+                       if let error = error {
+                           seal.reject(error)
+                       } else {
+                           switch status {
+                           case .successFetchedFromRemote, .successUsingPreFetchedData:
+                               do {
+                                   let diagnosis: DiagnosisKeyDownloadConfiguration = try self.decodeConfiguartion(key: .diagnosisKeyConfig)
+                                   let exposure: ExposureConfiguration = try self.decodeConfiguartion(key: .exposureConfiguration)
+                                   seal.fulfill(RemoteConfigurationResponse(diagnosis: diagnosis, exposure: exposure))
+                               } catch {
+                                   seal.reject(error)
+                               }
+                           case .error:
+                               seal.reject(InternalError.remoteActivate)
+                           @unknown default:
+                               seal.reject(InternalError.remoteUnknownStatus)
+                           }
+                       }
+                   }
+               }
     }
     
   private func decodeConfiguartion<T: Decodable>(key: Key) throws -> T {
