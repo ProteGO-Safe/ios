7e1ee18e6dcf9c59e1e8b69eb7e259a7ef28adef
lukasz szyszkowski <lukasz.szyszkowski@holdapp.pl>
2020-06-07 17:15:25 +0200

    [] Background task cleaning

--- safesafe/Services/ExposureNotification/BackgroundTasksService.swift
+++ safesafe/Services/ExposureNotification/BackgroundTasksService.swift
@@ -6,7 +6,6 @@
 //
 
 import BackgroundTasks
-import UserNotifications
 import UIKit.UIDevice
 
 protocol BackgroundTasksServiceProtocol {
@@ -53,48 +52,19 @@ final class BackgroundTasksService: BackgroundTasksServiceProtocol {
                 return
             }
             task.expirationHandler = {
-                #if STAGE
-                self?.scheduleNotification(body: "Task timeout: \(Date().timeIntervalSince1970)")
                 console("Task timed out", type: .warning)
-                #endif
             }
             
             self?.exposureService
                 .detectExposures()
                 .done { exposures in
-                    #if STAGE
-                    self?.scheduleNotification(body: "Task success: \(Date().timeIntervalSince1970)\nexposures: \(exposures.count)")
-                    #endif
                     task.setTaskCompleted(success: true)
                 }.catch {
-                    #if STAGE
-                    self?.scheduleNotification(body: "Task failure: \(Date().timeIntervalSince1970)\nexerror: \($0.localizedDescription)")
-                    #endif
                     console($0)
                     task.setTaskCompleted(success: true)
                 }.finally {
                     self?.scheduleExposureTask()
                 }
-        }
-    }
-    
-    func scheduleNotification(body: String) {
-
-        //Compose New Notificaion
-        let content = UNMutableNotificationContent()
-        let categoryIdentifire = "Delete Notification Type"
-        content.sound = UNNotificationSound.default
-        content.body = body
-        content.categoryIdentifier = categoryIdentifire
-
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let identifier = "Local Notification"
-        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-
-        UNUserNotificationCenter.current().add(request) { (error) in
-            if let error = error {
-                print("Error \(error.localizedDescription)")
-            }
         }
     }
 }
